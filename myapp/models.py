3538a3362e743bda51313e007b2179b92e786eef
cherif cherif <cheriff66@gmail.com>
2020-06-06 01:24:31 +0100

    model 2

--- myapp/models.py
+++ myapp/models.py
@@ -3,5 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    name = models.CharField(max_length=50)
-    age = models.IntegerField(max_length=50)
+
+    chosse=[
+            ('male','male'),
+            ('female','female'),
+            ('other','other')
+    ]
+    name = models.CharField(max_length=40, null=True, blank=True, unique=True, help_text='name please')
+    age = models.IntegerField(null=True, default='18')
+    gendre = models.CharField(null=True, max_length=10, choices=chosse)
+
+    def __str__(self):
+        return self.name
