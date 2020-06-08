0aaaae440ed9f5b8db31607f12c9241a59708b3f
cherif cherif <cheriff66@gmail.com>
2020-06-08 01:25:39 +0100

    moddels 4

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -9,9 +10,26 @@ class Student(models.Model):
             ('female','female'),
             ('other','other')
     ]
-    name = models.CharField(max_length=40, null=True, blank=True, unique=True, help_text='name please',editable=True, error_messages={"unique":"اسم محجوز"})
+    name = models.CharField(max_length=40, null=True, blank=True, unique=True, help_text='name please')
     age = models.IntegerField(null=True, default='18')
     gendre = models.CharField(null=True, max_length=10, choices=chosse)
+    ''' #x1 = models.BigIntegerField()
+    #x2 = models.AutoField()
+    #x3=  models.BigAutoField() 
+    x4 = models.BooleanField(name="check",null=True)
+    x41 = models.BooleanField()
+    x5 = models.DateField()   #timeField /datetimeField(deufault=datetime.now) '''
+    x1 = models.EmailField(name="Emaile", max_length=254, null=True)
+    x2 = models.FileField(upload_to="myapp/upload", max_length=100, null=True)
+    x3 = models.ImageField(upload_to="myapp/upload", null=True)
+    x4 = models.TextField(null=True)
+    
+
+    
+
+    
+    
+
 
     def __str__(self):
         return self.name
