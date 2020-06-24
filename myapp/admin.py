86d5af9ab39933f74ad41f63a72b4c96af7688c2
cherif cherif <cheriff66@gmail.com>
2020-06-24 16:17:28 +0100

    relations

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,4 +5,4 @@ from myapp import models
 # Register your models here.
 admin.site.register(models.Email)
 admin.site.register(models.Client)
-
+admin.site.register(models.Food)
--- myapp/models.py
+++ myapp/models.py
@@ -7,10 +7,18 @@ class Email(models.Model):
         user_email = models.EmailField(null=True)
         def __str__(self):
                 return self.user_email
-        
+
 class Client(models.Model):
         name = models.CharField(max_length=20, null=True)
         email = models.OneToOneField(Email, on_delete=models.CASCADE)
 
         def __str__(self):
             return self.name
+
+class Food(models.Model):
+    food_name = models.CharField(blank=True, max_length=100, null=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    #client = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return self.food_name
