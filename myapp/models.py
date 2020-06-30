0ee44b40101dc8c046de71e7486ad94c39e83af5
cherif cherif <cheriff66@gmail.com>
2020-06-30 17:38:57 +0100

    relation

--- myapp/models.py
+++ myapp/models.py
@@ -8,17 +8,25 @@ class Email(models.Model):
         def __str__(self):
                 return self.user_email
 
+
+class Food(models.Model):
+    food_name = models.CharField(blank=True, max_length=100, null=True)
+
+    def __str__(self):
+        return self.food_name
+
 class Client(models.Model):
         name = models.CharField(max_length=20, null=True)
         email = models.OneToOneField(Email, on_delete=models.CASCADE)
+        food = models.ForeignKey(Food, on_delete=models.CASCADE)
+        
 
         def __str__(self):
             return self.name
 
-class Food(models.Model):
-    food_name = models.CharField(blank=True, max_length=100, null=True)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    #client = models.ManyToManyField(Client)
 
-    def __str__(self):
-        return self.food_name
+
+
+    
+    
+    
